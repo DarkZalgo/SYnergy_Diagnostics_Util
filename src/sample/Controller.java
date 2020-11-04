28c06b2c834b206ac68d6fe38d09eeaa7aadbebe
darkzalgo <darkzalgo@gmail.com>
2020-11-03 20:27:00 -0700

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,306 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+
+public class Controller implements Initializable
+{
+    @FXML RadioButton SY_2416, SY_A20, SY_X;
+    @FXML RadioButton barcodeReader, hidReader, iClassReader, magneticReader, proximityReader, noneReader, mifareReader;
+    @FXML RadioButton casFPU, suprFPU, noneFPU;
+    @FXML RadioButton casThreeK, casTenK, suprNineK, suprTwentyFiveK;
+    @FXML RadioButton serialRadio, sshRadio;
+    @FXML RadioButton POEComm, ethComm;
+    @FXML RadioButton sdBoard, threeWire, twoWire, unmodified, A20mBoard;
+    @FXML RadioButton oneThreeA6H, oneThreeANon6H, oneThreeNon6H, oneTwo, A20cBoard;
+    @FXML RadioButton delkinRadio, hcRadio, otherCardRadio;
+    @FXML RadioButton blueBattery, silverBattery, noneBattery;
+
+    @FXML TextField connectionField, serialNumField, caseNumField, otherCardField;
+
+    @FXML CheckBox POEPowerBox, adapterPowerBox, neitherPowerBox;
+    @FXML CheckBox wifiCommBox, gprsBox;
+
+    @FXML Label connectionLabel;
+
+    ToggleGroup modelGroup = new ToggleGroup();
+    ToggleGroup readerGroup = new ToggleGroup();
+    ToggleGroup fpuTypeGroup = new ToggleGroup();
+    ToggleGroup fpuSizeGroup = new ToggleGroup();
+    ToggleGroup connectionGroup = new ToggleGroup();
+    ToggleGroup communicationGroup = new ToggleGroup();
+    ToggleGroup coreboardGroup = new ToggleGroup();
+    ToggleGroup motherboardGroup = new ToggleGroup();
+    ToggleGroup sdCardGroup = new ToggleGroup();
+    ToggleGroup batteryGroup = new ToggleGroup();
+
+    Set<RadioButton> modelSet;
+    Set<RadioButton> readerSet;
+    Set<RadioButton> fpuTypeSet;
+    Set<RadioButton> fpuSizeSet;
+    Set<RadioButton> connectionSet;
+    Set<RadioButton> communicationSet;
+    Set<RadioButton> coreboardSet;
+    Set<RadioButton> motherboardSet;
+    Set<RadioButton> sdCardSet;
+    Set<RadioButton> batterySet;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+        modelSet = new HashSet<>(Arrays.asList(SY_2416, SY_A20, SY_X));
+        readerSet = new HashSet<>(Arrays.asList(barcodeReader, hidReader, iClassReader, magneticReader, proximityReader, noneReader, mifareReader));
+        fpuTypeSet = new HashSet<>(Arrays.asList(casFPU, suprFPU, noneFPU));
+        fpuSizeSet = new HashSet<>(Arrays.asList(casThreeK, casTenK, suprNineK, suprTwentyFiveK));
+        connectionSet = new HashSet<>(Arrays.asList(serialRadio, sshRadio));
+        communicationSet = new HashSet<>(Arrays.asList(POEComm, ethComm));
+        coreboardSet = new HashSet<>(Arrays.asList(sdBoard, threeWire, twoWire, unmodified, A20mBoard));
+        motherboardSet = new HashSet<>(Arrays.asList(oneThreeA6H, oneThreeANon6H, oneThreeNon6H, oneTwo, A20cBoard));
+        sdCardSet = new HashSet<>(Arrays.asList(delkinRadio, hcRadio, otherCardRadio));
+        batterySet = new HashSet<>(Arrays.asList(blueBattery, silverBattery, noneBattery));
+
+
+
+        for(RadioButton modelRadio : modelSet)
+        {
+            modelRadio.setToggleGroup(modelGroup);
+        }
+
+        for(RadioButton readerRadio : readerSet)
+        {
+            readerRadio.setToggleGroup(readerGroup);
+        }
+
+        for(RadioButton fpuTypeRadio : fpuTypeSet)
+        {
+            fpuTypeRadio.setToggleGroup(fpuTypeGroup);
+        }
+
+        for(RadioButton fpuSizeRadio : fpuSizeSet)
+        {
+            fpuSizeRadio.setToggleGroup(fpuSizeGroup);
+        }
+
+        for(RadioButton connectionRadio : connectionSet)
+        {
+            connectionRadio.setToggleGroup(connectionGroup);
+        }
+
+        for(RadioButton commRadio : communicationSet)
+        {
+            commRadio.setToggleGroup(communicationGroup);
+        }
+
+        for(RadioButton mbRadio : motherboardSet)
+        {
+            mbRadio.setToggleGroup(motherboardGroup);
+        }
+
+        for(RadioButton cbRadio : coreboardSet)
+        {
+            cbRadio.setToggleGroup(coreboardGroup);
+        }
+
+        for(RadioButton sdRadio : sdCardSet)
+        {
+            sdRadio.setToggleGroup(sdCardGroup);
+        }
+
+        for(RadioButton batteryRadio : batterySet)
+        {
+            batteryRadio.setToggleGroup(batteryGroup);
+        }
+
+
+        casThreeK.setVisible(false);
+        casTenK.setVisible(false);
+        suprNineK.setVisible(false);
+        suprTwentyFiveK.setVisible(false);
+
+
+        sdBoard.setVisible(false);
+        threeWire.setVisible(false);
+        twoWire.setVisible(false);
+        unmodified.setVisible(false);
+        A20mBoard.setVisible(false);
+
+        oneThreeA6H.setVisible(false);
+        oneThreeANon6H.setVisible(false);
+        oneThreeNon6H.setVisible(false);
+        oneTwo.setVisible(false);
+        A20cBoard.setVisible(false);
+
+        connectionLabel.setVisible(false);
+
+        modelGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1)
+            {
+                if (modelGroup.getSelectedToggle() == SY_A20 || modelGroup.getSelectedToggle() == SY_X)
+                {
+                    A20cBoard.setVisible(true);
+                    A20mBoard.setVisible(true);
+
+                    sdBoard.setVisible(false);
+                    threeWire.setVisible(false);
+                    twoWire.setVisible(false);
+                    unmodified.setVisible(false);
+
+                    oneThreeA6H.setVisible(false);
+                    oneThreeANon6H.setVisible(false);
+                    oneThreeNon6H.setVisible(false);
+                    oneTwo.setVisible(false);
+                }
+                else if(modelGroup.getSelectedToggle() == SY_2416)
+                {
+                    A20cBoard.setVisible(false);
+                    A20mBoard.setVisible(false);
+
+                    sdBoard.setVisible(true);
+                    threeWire.setVisible(true);
+                    twoWire.setVisible(true);
+                    unmodified.setVisible(true);
+
+                    oneThreeA6H.setVisible(true);
+                    oneThreeANon6H.setVisible(true);
+                    oneThreeNon6H.setVisible(true);
+                    oneTwo.setVisible(true);
+                }
+            }
+        });
+
+        fpuTypeGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1)
+            {
+                if (fpuTypeGroup.getSelectedToggle() == casFPU)
+                {
+                    casThreeK.setVisible(true);
+                    casTenK.setVisible(true);
+
+                    suprNineK.setVisible(false);
+                    suprTwentyFiveK.setVisible(false);
+                }
+                else if(fpuTypeGroup.getSelectedToggle() == suprFPU)
+                {
+                    casThreeK.setVisible(false);
+                    casTenK.setVisible(false);
+
+                    suprNineK.setVisible(true);
+                    suprTwentyFiveK.setVisible(true);
+                }
+                else if(fpuTypeGroup.getSelectedToggle() == noneFPU)
+                {
+                    casThreeK.setVisible(false);
+                    casTenK.setVisible(false);
+
+                    suprNineK.setVisible(false);
+                    suprTwentyFiveK.setVisible(false);
+                }
+            }
+        });
+
+        connectionGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1)
+            {
+                if (connectionGroup.getSelectedToggle() == sshRadio)
+                {
+                    connectionLabel.setText("Enter IP Address");
+                    connectionLabel.setVisible(true);
+                }
+                else if(connectionGroup.getSelectedToggle() == serialRadio)
+                {
+                    connectionLabel.setText("Enter COM Port");
+                    connectionLabel.setVisible(true);
+                }
+            }
+        });
+
+    }
+
+    @FXML
+    private void refreshAll()
+    {
+        for(RadioButton modelRadio : modelSet)
+        {
+            modelRadio.setSelected(false);
+        }
+
+        for(RadioButton readerRadio : readerSet)
+        {
+            readerRadio.setSelected(false);
+        }
+
+        for(RadioButton fpuTypeRadio : fpuTypeSet)
+        {
+            fpuTypeRadio.setSelected(false);
+        }
+
+        for(RadioButton fpuSizeRadio : fpuSizeSet)
+        {
+            fpuSizeRadio.setSelected(false);
+        }
+
+        for(RadioButton connectionRadio : connectionSet)
+        {
+            connectionRadio.setSelected(false);
+        }
+
+        for(RadioButton commRadio : communicationSet)
+        {
+            commRadio.setSelected(false);
+        }
+
+        for(RadioButton mbRadio : motherboardSet)
+        {
+            mbRadio.setSelected(false);
+        }
+
+        for(RadioButton cbRadio : coreboardSet)
+        {
+            cbRadio.setSelected(false);
+        }
+
+        for(RadioButton sdRadio : sdCardSet)
+        {
+            sdRadio.setSelected(false);
+        }
+
+        for(RadioButton batteryRadio : batterySet)
+        {
+            batteryRadio.setSelected(false);
+        }
+
+
+        caseNumField.setText("");
+        serialNumField.setText("");
+        connectionField.setText("");
+        otherCardField.setText("");
+
+
+        wifiCommBox.setSelected(false);
+        gprsBox.setSelected(false);
+
+        POEPowerBox.setSelected(false);
+        adapterPowerBox.setSelected(false);
+        neitherPowerBox.setSelected(false);
+    }
+}
